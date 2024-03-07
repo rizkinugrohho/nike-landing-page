a4e4653683d3fe0caa5ddba1838249960ca10561
A A S <rizkynhg@gmail.com>
2024-03-07 15:05:43 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nike-landing-tailwind',
+  base: '/nike-landing',
   plugins: [react()],
 });
