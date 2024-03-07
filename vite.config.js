89f5e49817ac710b4205367005e97370b8db87a5
A A S <rizkynhg@gmail.com>
2024-03-07 15:09:34 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nike-landing',
+  base: '/nike-landing-page',
   plugins: [react()],
 });
